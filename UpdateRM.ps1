dbb7259d28dceae913e2ffd459b8539a6ceb3715
Maicon Santos <maiconcongesco@gmail.com>
2020-05-08 12:31:22 -0300

    Inclusão de Variavies para o nome do serviço

--- UpdateRM.ps1
+++ UpdateRM.ps1
@@ -33,8 +33,9 @@ $DIRsvcRM = "C:\Program Files (x86)\RiskManager.Service"
 $DIRsvcScheduler = "C:\Program Files (x86)\Modulo Scheduler Service"
 $DIRbkpfullRM = "D:\BackupRM"
 $DIRsiteRM = "D:\RiskManager"
-$FileWebApp = "E:\RM_9.8.9.01\Web_Applications"
 $PackInstallRM = "D:\FilesRiskManager\RM_9.9.2.7\"
+$ModuloSchedulerService = "ModuloSchedulerService"
+$RiskManagerService =  "RiskManagerService"
 #$helper = New-Object -ComObject Shell.
 #$Destination = "C:\pastadestino"
 #$Source = "C:\arquivo.zip"
@@ -43,8 +44,9 @@ $PackInstallRM = "D:\FilesRiskManager\RM_9.9.2.7\"
 #===========================================================================================#
 #   Parando os serviços Modulo Scheduler e Risk Manager
 #===========================================================================================#
+Get-Service -DisplayName "Modulo*", "Risk*" # Verificando nome do serviço
 
-Get-Service -DisplayName "Modulo Scheduler Service", "Risk Manager Service" | Stop-Service
+Get-Service -DisplayName "$ModuloSchedulerService", "$RiskManagerService" | Stop-Service
 
 #===========================================================================================#
 #   Parando os WebAppPools
@@ -59,11 +61,6 @@ Stop-WebAppPool "DataAnalyticsUI" # *> "$destinyPath\log-$date.txt"
 Stop-WebAppPool "MMI" # *> "$destinyPath\log-$date.txt"
 Stop-WebAppPool "BCM" # *> "$destinyPath\log-$date.txt"
 #>
-
-#===========================================================================================#
-#	Removendo a Restrição de uso do script
-#===========================================================================================#
-# Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 
 #===========================================================================================#
 #	Limpando os Logs		>>> ATENÇÃO! Essa remoção pode ser irreversível
@@ -157,35 +154,35 @@ New-Service -Name "ModuloSchedulerService" -BinaryPathName Modulo.Scheduler.Host
 Set-Location "C:\Program Files\IIS\Microsoft Web Deploy V3"
 
 # Deploy da aplicação RM  
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\RM.WebApplication.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/RM"
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\RM.WebApplication.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/RM"
 
 # Deploy da aplicação Workflow
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\Workflow.Services.Web.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/WF"
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\Workflow.Services.Web.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/WF"
 
 # Deploy da aplicação PORTAL  
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\RM.PORTAL.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/PORTAL"
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\RM.PORTAL.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/PORTAL"
 
 # Deploy da aplicação Data Analytics Cacher 
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\DataAnalytics\DataAnalyticsCacher.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/DataAnalyticsCacher"
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\DataAnalytics\DataAnalyticsCacher.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/DataAnalyticsCacher"
 
 # Deploy da aplicação Data Analytics Service 
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\DataAnalytics\DataAnalyticsService.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/DataAnalyticsService" 
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\DataAnalytics\DataAnalyticsService.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/DataAnalyticsService" 
 
 # Deploy da aplicação Data Analytics UI 
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\DataAnalytics\DataAnalyticsUI.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/DataAnalyticsUI"
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\DataAnalytics\DataAnalyticsUI.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/DataAnalyticsUI"
 
 # Deploy da aplicação Data MMI 
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\MMI\packages\Modulo.SICC.WebApplication.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/MMI" 
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\MMI\packages\Modulo.SICC.WebApplication.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/MMI" 
 
 # Deploy da aplicação BCM 
-.\msdeploy.exe -verb=sync -source:package="$FileWebApp\Web.Applications\BCM.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/BCM"
+.\msdeploy.exe -verb=sync -source:package="$PackInstallRM\Web.Applications\BCM.zip" -dest:Auto -setParam:"IIS Web Application Name""=RiskManager/BCM"
 #>
 
 #===========================================================================================#
 #   Passos a incluir no script
 #===========================================================================================#
 
-# Cópia da biblioteca DevExpress
+# 
 # Cópia do arquivo de licença
 # Cópia do Manual do Módulo Risk Manager
 # Cópia do arquivo Modulo.RiskManager.DataAnalytics.Bootstrap
