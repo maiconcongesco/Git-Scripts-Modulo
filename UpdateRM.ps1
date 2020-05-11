eaf81dbd78372164f0adef919248369c58642657
Maicon Santos <maiconcongesco@gmail.com>
2020-05-11 02:44:49 -0300

    Ativando verbose e classificando a frequencia de alterações na variaveis

--- UpdateRM.ps1
+++ UpdateRM.ps1
@@ -25,31 +25,42 @@ Set-ExecutionPolicy Restricted ### O PowerShell só pode ser usado no modo inter
 Set-ExecutionPolicy Unrestricted ###  Todos os scripts do Windows PowerShell podem ser executados.
 Set-ExecutionPolicy AllSigned ### Somente scripts assinados por um editor confiável podem ser executados.
 Set-ExecutionPolicy RemoteSigned ### Os scripts baixados devem ser assinados por um editor confiável antes que possam ser executados.
-# ===========================================================================================#>
+#>
 
+#===========================================================================================#
+#   Variáveis >>> ATENÇÃO: Um erro no preenchimento dessas variaveis e todo o script é comprometido
+#===========================================================================================#
+
+# Geralmente essas váriaveis precisarão ser alteradas
 $VersionBKPdoRM = "9.9.2.7" # Versão do RM que será arquivado (Backup)
-$DirBKPConfgs = "D:\BackupRM\Configs\$VersionBKPdoRM"
-$DIRbkpfullRM = "D:\BackupRM\$VersionBKPdoRM" # Diretório onde faremos o Backup de todo o conteúdo dos serviços e sites do Risk Manager, se ela não existir o script a criará.
-$FileLicense = "D:\BackupRM\RiskManager\RM\modulelicenses.config" # Caminho do Arquivo de licença do RiskManager.
+$DIRbkpfull = "D:\BackupRM" # Diretório de backup do Risk Manager
 $FileManual = "Manual Versao 9.9 22.04.2020_v2.zip" # Versão do arquivo de licença do Manual.
-$XDays = 00  # Quantidade de dias que pretende reter o log.
-$Extensions	= "*.slog" #  Separe por virgula as extensões dos arquivos que serão deletados.
-$DIRsvcRM = "C:\Program Files (x86)\RiskManager.Service" # Diretório do Serviço do Risk Manager.
-$DIRsvcScheduler = "C:\Program Files (x86)\Modulo Scheduler Service" # Diretório do Serviço do Modulo Scheduler.
 $DIRsiteRM = "D:\RiskManager" # Diretório do Site do Risk Manager
 $PackInstallRM = "D:\FilesRiskManager\RM_9.9.2.7" # Diretório com os arquivos de atualização do Risk Manager
+
+# Ocasionalmente pode ser necessário alterar essa variáveis
+$DIRsvcRM = "C:\Program Files (x86)\RiskManager.Service" # Diretório do Serviço do Risk Manager.
+$DIRsvcScheduler = "C:\Program Files (x86)\Modulo Scheduler Service" # Diretório do Serviço do Modulo Scheduler.
+$ModuloSchedulerService = "ModuloSchedulerService" # Execute o comando [Get-Service -DisplayName "Modulo*", "Risk*"] sem os "[]" para descobrir o Nome do Serviço do Modulo Scheduler >>> ATENÇÃO: Esse nome deve está correto, caso contrário o script não irá excluir o serviço antigo.
+$RiskManagerService =  "RiskManagerService" # Execute o comando [Get-Service -DisplayName "Modulo*", "Risk*"] sem os "[]" para descobrir o Nome do Serviço do Risk Manager >>> ATENÇÃO: Esse nome deve está correto, caso contrário o script não irá excluir o serviço antigo.
+
+# Raramente será necessário alterar essa variáveis
+$DirBKPConfgs = "$DIRbkpfull\Configs\$VersionBKPdoRM" # Diretório de backup dos configs do Risk Manager
+$DIRbkpfullRM = "$DIRbkpfull\$VersionBKPdoRM" # Diretório onde faremos o Backup de todo o conteúdo dos serviços e sites do Risk Manager, se ela não existir o script a criará.
+$FileLicense = "$DIRbkpfullRM\LicenseRM\modulelicenses.config" # Caminho do Arquivo de licença do RiskManager.
+$XDays = 00  # Quantidade de dias que pretende reter o log.
+$Extensions	= "*.slog" #  Separe por virgula as extensões dos arquivos que serão deletados.
 $LogPath = "$DIRsvcRM, $DIRsvcScheduler, $DIRsiteRM"   # Caminho da pasta principal onde iremos buscar e limpar os logs, Separe por virgula se for mais de uma pasta.
-$ModuloSchedulerService = "ModuloSchedulerService" # Nome existente do Serviço do Modulo Scheduler >>> ATENÇÃO: Esse nome deve está correto, caso contrário o script não irá excluir o serviço antigo.
-$RiskManagerService =  "RiskManagerService" # Nome existente do Serviço do Risk Manager >>> ATENÇÃO: Esse nome deve está correto, caso contrário o script não irá excluir o serviço antigo.
-#$helper = New-Object -ComObject Shell.
-#$Destination = "C:\pastadestino"
-#$Source = "C:\arquivo.zip"
-#$files = $helper.NameSpace($Source).Items()
+
+#===========================================================================================#
+
+# Inicio da execução do Script
+$command = Get-History -Count 1 # Vai Cronometrar o tempo que o script levará em execução
+$command.StartExecutionTime 
 
 #===========================================================================================#
 #   Parando os serviços Modulo Scheduler e Risk Manager
 #===========================================================================================#
-#Get-Service -DisplayName "Modulo*", "Risk*" # Verificando nome do serviço
 
 Get-Service -Name "$ModuloSchedulerService", "$RiskManagerService" | Stop-Service
 
@@ -126,15 +137,20 @@ If(!(test-path $DirBKPConfgs))
 #===========================================================================================#
 
 #Copia os arquivos e a estrutura de Diretórios.
-Copy-Item "$DIRbkpfullRM" -Filter "*.config" -Destination "$DirBKPConfgs" -Recurse -Force
+Copy-Item "$DIRbkpfullRM" -Filter "*.config" -Destination "$DirBKPConfgs" -Recurse -Force -Verbose
 
 #Get-ChildItem -Path  "$DirBKPConfgs" -Recurse -exclude "*.config" | Remove-Item -force -recurse #Matem o *config
 
-(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item
-(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item
-(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item
-(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item
-(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item
+(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item -Verbose
+(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item -Verbose
+(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item -Verbose
+(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item -Verbose
+(Get-ChildItem “$DirBKPConfgs” -r | Where-Object {$_.PSIsContainer -eq $True}) | Where-Object{$_.GetFileSystemInfos().Count -eq 0} | remove-item -Verbose
+
+#===========================================================================================#
+#   Backup do arquivo de lincença do Risk Manager
+#===========================================================================================#
+Copy-Item "$DIRsiteRM\RM" -Filter "modulelicenses*.config" -Destination "$DIRbkpfullRM\LicenseRM" -Recurse -Force -Verbose
 
 #===========================================================================================#
 #   Remover conteudo das pastas dos Serviços e APPs
@@ -147,22 +163,22 @@ Remove-Item -Path $DIRsiteRM\* -Recurse -Verbose -Force # *> "$destinyPath\log-$
 #===========================================================================================#
 #   Renomeando os arquivos dos serviços Risk Manager e Modulo Scheduler
 #===========================================================================================#
-rename-item -path "$PackInstallRM\Binaries\Modulo Scheduler Service.zipx" -newname "Modulo Scheduler Service.zip" # *> "$destinyPath\log-$date.txt"
-rename-item -path "$PackInstallRM\Binaries\RiskManager.Service.zipx" -newname "RiskManager.Service.zip" # *> "$destinyPath\log-$date.txt"
+rename-item -path "$PackInstallRM\Binaries\Modulo Scheduler Service.zipx" -newname "Modulo Scheduler Service.zip" -Verbose # *> "$destinyPath\log-$date.txt"
+rename-item -path "$PackInstallRM\Binaries\RiskManager.Service.zipx" -newname "RiskManager.Service.zip" -Verbose # *> "$destinyPath\log-$date.txt"
 #>
 
 #===========================================================================================#
 #   Descompactando os arquivos dos serviços Risk Manager e Modulo Scheduler
 #===========================================================================================#
-Expand-Archive -Path "$PackInstallRM\Binaries\Modulo Scheduler Service.zip" -DestinationPath $DIRsvcScheduler
-Expand-Archive -Path "$PackInstallRM\Binaries\RiskManager.Service.zip" -DestinationPath $DIRsvcRM
+Expand-Archive -Path "$PackInstallRM\Binaries\Modulo Scheduler Service.zip" -DestinationPath $DIRsvcScheduler -Verbose
+Expand-Archive -Path "$PackInstallRM\Binaries\RiskManager.Service.zip" -DestinationPath $DIRsvcRM -Verbose
 #>
 
 #===========================================================================================#
 #    Recriando os serviços Risk Manager e Modulo Scheduler
 #===========================================================================================#
-New-Service -Name "RiskManagerService" -BinaryPathName "$DIRsvcRM/RM.Service.exe"
-New-Service -Name "ModuloSchedulerService" -BinaryPathName "$DIRsvcRM/Modulo.Scheduler.Host.exe"
+New-Service -BinaryPathName $DIRsvcRM/RM.Service.exe -Name RiskManagerService -Description "Risk Manager Background Service Host" -DisplayName "Risk Manager Service" -Verbose
+New-Service -BinaryPathName $DIRsvcRM/Modulo.Scheduler.Host.exe -Name ModuloSchedulerService -Description "Modulo Scheduler Background Service Host" -DisplayName "Modulo Scheduler Service" -Verbose
 #>
 
 #===========================================================================================#
@@ -201,28 +217,28 @@ Set-Location "C:\Program Files\IIS\Microsoft Web Deploy V3"
 #   Cópia da biblioteca DevExpress para Apps/bin
 #===========================================================================================#
 
-Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\RM\bin" -Force
-Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\WF\bin" -Force
-Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\PORTAL\bin" -Force
-Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\BCM\bin" -Force
+Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\RM\bin" -Force -Verbose
+Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\WF\bin" -Force -Verbose
+Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\PORTAL\bin" -Force -Verbose
+Copy-Item -Path "$PackInstallRM\DevExpress\*.dll" -Destination "$DIRsiteRM\BCM\bin" -Force -Verbose
 
 #===========================================================================================#
 #   Cópia do arquivo Modulo.RiskManager.DataAnalytics.Bootstrap
 #===========================================================================================#
 
-Copy-Item -Path "$PackInstallRM\Web.Applications\DataAnalytics\Modulo.RiskManager.DataAnalytics.Bootstrap.dll" -Destination "$DIRsiteRM\RM\bin" -Force
+Copy-Item -Path "$PackInstallRM\Web.Applications\DataAnalytics\Modulo.RiskManager.DataAnalytics.Bootstrap.dll" -Destination "$DIRsiteRM\RM\bin" -Force -Verbose
 
 #===========================================================================================#
 #   Cópia do conteúdo da pasta do pacote Data Analytics\DashboardDesignerInstallers
 #===========================================================================================#
 
-Copy-Item -Path "$PackInstallRM\Web.Applications\DataAnalytics\DashboardDesignerInstallers\*" -Destination "$DIRsiteRM\DataAnalyticsUI\Files" -Force
+Copy-Item -Path "$PackInstallRM\Web.Applications\DataAnalytics\DashboardDesignerInstallers\*" -Destination "$DIRsiteRM\DataAnalyticsUI\Files" -Force -Verbose
 
 #===========================================================================================#
 #   Cópia do arquivo de licença
 #===========================================================================================#
 
-Copy-Item -Path "$FileLicense"  -Destination "$DIRsiteRM\RM" -Force
+Copy-Item -Path "$FileLicense"  -Destination "$DIRsiteRM\RM" -Force -Verbose
 
 #===========================================================================================#
 #   Criando o diretório para o Manual
@@ -230,7 +246,7 @@ Copy-Item -Path "$FileLicense"  -Destination "$DIRsiteRM\RM" -Force
 
 If(!(test-path $DIRsiteRM\RM\Manual))
 {
-      New-Item -ItemType Directory -Force -Path $DIRsiteRM\RM\Manual
+      New-Item -ItemType Directory -Force -Path $DIRsiteRM\RM\Manual -Verbose
 }
 #>
 
@@ -239,7 +255,7 @@ If(!(test-path $DIRsiteRM\RM\Manual))
 #===========================================================================================#
 
 # No Powershell v5 você pode utilizar os seguintes cmdlets pra descompactar.
-Expand-Archive -Path "$PackInstallRM\$FileManual" -DestinationPath "$DIRsiteRM\RM\Manual"
+Expand-Archive -Path "$PackInstallRM\$FileManual" -DestinationPath "$DIRsiteRM\RM\Manual" -Verbose
 #>
 
 #===========================================================================================#
@@ -257,3 +273,5 @@ Start-WebAppPool "DataAnalyticsUI" # *> "$destinyPath\log-$date.txt"
 Start-WebAppPool "MMI" # *> "$destinyPath\log-$date.txt"
 Start-WebAppPool "BCM" # *> "$destinyPath\log-$date.txt"
 #>
+
+$command.EndExecutionTime # Inicio da execução do Script
