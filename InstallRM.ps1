7b60e4a7efc24bd844bc2485ad832e91a8c6f6cb
Maicon <maiconcongesco@gmail.com>
2020-05-26 13:10:47 -0300

    Notas de versões do Powershell e sua integração e compatibilidade com Windows e Windows Server

--- InstallRM.ps1
+++ InstallRM.ps1
@@ -14,12 +14,23 @@
 #===========================================================================================#
 #   Necessário instalar o Microsoft Web Deploy V3 contido no "Pack Tools"
 #===========================================================================================#
+#===========================================================================================#
+#### Versões do Powershell e sua integração e compatibilidade com Windows e Windows Server ####
+Powershell 1.0 — Foi feito para Windows XP SP2, Windows Server 2003 SP1 e Windows Vista. E é um componente opcional para Windows Server 2008.
+Powershell 2.0 — Integrado com o Windows 7 e Windows Server 2008 R2. No Windows XP está disponível com o SP3, para o Windows Server 2003 com o SP2, e Windows Vista com o SP1.
+Powershell 3.0 — Integrado com o Windows 8 e Windows Server 2012. No Windows 7, Windows Server 2008 e Windows Server 2008 R2 está disponível nos respectivos SP1.
+Powershell 4.0 — Integrado com o Windows 8.1 e com o Windows Server 2012 R2. No Windows 7, Windows Server 2008 R2 e Windows Server 2012 está disponível nos respectivos SP1.
+Powershell 5.0 — Integrado com o Windows Server 2016 e Windows 10 (no update de aniversário). A compatibilidade com o Windows Vista, Windows 7, Windows Server 2008, Windows Server 2008 R2, Windows Server 2012 e Windows Server 2012 R2 foi lançada em 19 de janeiro de 2017.
+#===========================================================================================#
+#===========================================================================================#
 # Em algumas situações pode ser necessário alterar a diretiva de execução de script do Windows.
 O cmdlet "Set-ExecutionPolicy" determina se os scripts PowerShell terão permissão de execução. O cmdlet "Get-ExecutionPolicy" verifica a política de execução corrente, o cmdlet aplica Set-ExecutionPolicy a política.
 Set-ExecutionPolicy Restricted ### O PowerShell só pode ser usado no modo interativo. O script não poderá pode ser executado. 
 Set-ExecutionPolicy Unrestricted ###  Todos os scripts do Windows PowerShell podem ser executados.
 Set-ExecutionPolicy AllSigned ### Somente scripts assinados por um editor confiável podem ser executados.
 Set-ExecutionPolicy RemoteSigned ### Os scripts baixados devem ser assinados por um editor confiável antes que possam ser executados.
+#===========================================================================================#
+#===========================================================================================#
 #>
 
 #===========================================================================================#
