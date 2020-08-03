abce998a0491a07a5e164d89d8942f4f24b2c34d
Maicon <maiconcongesco@gmail.com>
2020-08-03 18:26:55 -0300

    Carregando Script de restart do Risk Manager

---
+++ RestartRM.ps1
@@ -0,0 +1,81 @@
+##
+<#===========================================================================================#>
+<#===========================================================================================#>
+<#
+<#  Restart Risk Manager
+<#  Autor: Maicon Santos
+<#
+<#===========================================================================================#>
+<#===========================================================================================#>
+
+<#==========================================================================================#>
+<#  >>> OBSERVAÇÕES IMPORTANTES <<<
+<#===========================================================================================#>
+<#  Necessário instalar o Microsoft Web Deploy V3 contido no "Pack Tools"
+<#===========================================================================================#>
+<#===========================================================================================#>
+<### Versões do Powershell e sua integração e compatibilidade com Windows e Windows Server ###
+Powershell 1.0 — Foi feito para Windows XP SP2, Windows Server 2003 SP1 e Windows Vista. E é um componente opcional para Windows Server 2008.
+Powershell 2.0 — Integrado com o Windows 7 e Windows Server 2008 R2. No Windows XP está disponível com o SP3, para o Windows Server 2003 com o SP2, e Windows Vista com o SP1.
+Powershell 3.0 — Integrado com o Windows 8 e Windows Server 2012. No Windows 7, Windows Server 2008 e Windows Server 2008 R2 está disponível nos respectivos SP1.
+Powershell 4.0 — Integrado com o Windows 8.1 e com o Windows Server 2012 R2. No Windows 7, Windows Server 2008 R2 e Windows Server 2012 está disponível nos respectivos SP1.
+Powershell 5.0 — Integrado com o Windows Server 2016, 2019 e Windows 10 (no update de aniversário). A compatibilidade com o Windows Vista e Seven (7), Windows Server 2008, 2008 R2, 2012 e 2012 R2.
+<#===========================================================================================#>
+<#===========================================================================================#>
+<# Em algumas situações pode ser necessário alterar a diretiva de execução de script do Windows.
+O cmdlet "Set-ExecutionPolicy" determina se os scripts PowerShell terão permissão de execução. O cmdlet "Get-ExecutionPolicy" verifica a política de execução corrente, o cmdlet aplica Set-ExecutionPolicy a política.
+Set-ExecutionPolicy Restricted ### O PowerShell só pode ser usado no modo interativo. O script não poderá pode ser executado. 
+Set-ExecutionPolicy Unrestricted ##<# Todos os scripts do Windows PowerShell podem ser executados.
+Set-ExecutionPolicy AllSigned ### Somente scripts assinados por um editor confiável podem ser executados.
+Set-ExecutionPolicy RemoteSigned ### Os scripts baixados devem ser assinados por um editor confiável antes que possam ser executados.
+<#===========================================================================================#>
+<#===========================================================================================#>
+
+#===========================================================================================#
+#   Parando os WebAppPools
+#===========================================================================================#
+Stop-WebAppPool "RiskManager" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "RM" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "PORTAL" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "WF" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "DataAnalyticsCacher" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "DataAnalyticsService" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "DataAnalyticsUI" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "MMI" # *> "$destinyPath\log-$date.txt"
+Stop-WebAppPool "BCM" # *> "$destinyPath\log-$date.txt"
+#Stop-WebAppPool "ETLProcessor" # *> "$destinyPath\log-$date.txt"
+#>
+
+#===========================================================================================#
+#   Iniciando os WebAppPool
+#===========================================================================================# 
+Start-WebAppPool "RiskManager" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "RM" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "PORTAL" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "WF" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "DataAnalyticsCacher" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "DataAnalyticsService" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "DataAnalyticsUI" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "MMI" # *> "$destinyPath\log-$date.txt"
+Start-WebAppPool "BCM" # *> "$destinyPath\log-$date.txt"
+#Start-WebAppPool "ETLProcessor" # *> "$destinyPath\log-$date.txt"
+#>
+
+#===========================================================================================#
+#   Verifcando os serviços Modulo Scheduler e Risk Manager
+#===========================================================================================#
+Get-Service -Name "Modulo*", "Risk*"
+
+#===========================================================================================#
+#   Parando os serviços Modulo Scheduler e Risk Manager
+#===========================================================================================#
+Get-Service -Name "RiskManagerService" | Stop-Service
+Get-Service -Name "ModuloSchedulerService" | Stop-Service
+#
+
+#===========================================================================================#
+#   Iniciando os serviços Modulo Scheduler e Risk Manager
+#===========================================================================================#
+Get-Service -Name "RiskManagerService" | Start-Service
+Get-Service -Name "ModuloSchedulerService" | Start-Service
+#
