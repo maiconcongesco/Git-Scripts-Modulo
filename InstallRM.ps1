2847d945bf8c3d3623860328b01effeffaf81a73
Maicon <maiconcongesco@users.noreply.github.com>
2020-10-19 15:02:31 -0300

    Inseri informaçãoes de Requisito de Hardware

--- InstallRM.ps1
+++ InstallRM.ps1
@@ -73,15 +73,14 @@ $PSVersionTable
 # Add-WindowsFeature web-server, web-webserver, web-common-http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Http-Redirect,  Web-Health, Web-Http-Logging, Web-Log-Libraries, Web-Request-Monitor, Web-Http-Tracing, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering,  Web-App-Dev, Web-Net-Ext45, Web-AppInit, Web-ASP, Web-Asp-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Mgmt-Console, Web-Mgmt-Compat, Web-Metabase, Web-Scripting-Tools, MSMQ-Services, MSMQ-Server, MSMQ-Directory, MSMQ-HTTP-Support, MSMQ-Triggers,  NET-Framework-45-Features, NET-Framework-45-Core, NET-Framework-45-ASPNET, NET-WCF-Services45, NET-WCF-TCP-PortSharing45
 
 <#===========================================================================================#>
-<#  Recurso de memoria no Servidor em GB
+<#  Recurso de memoria no Servidor em GB | Requisito Recomendado é 12GB
 <#===========================================================================================#>
 # "{0:N2}" -f ((Get-Counter -Counter "\Memory\Available Bytes").CounterSamples.CookedValue / 1GB)
 
 <#===========================================================================================#>
-<#  Recurso de CPU
+<#  Recurso de CPU | Requisito Recomendado é 8vCPU
 <#===========================================================================================#>
 # Get-WmiObject -Class Win32_Processor | Select-Object -Property PSComputerName, Name, NumberOfCores, ThreadCount   
-
 
 <#===========================================================================================#>
 <#  Instalando o WebDeploy		         				
@@ -278,7 +277,6 @@ If(!(test-path $DIRsiteRM\RM\Manual\pt))
 <#===========================================================================================#>
 Expand-Archive -Path "$FileManual" -DestinationPath "$DIRsiteRM\RM\Manual\pt" -Force -Verbose
 #>
-
 
 <#===========================================================================================#>
 <#  Alterando extensão dos arquivos dos serviços Risk Manager e Modulo Scheduler
