dcd6a1731d9f4f0db779534da90473d81dc9dfcc
Maicon <maiconcongesco@users.noreply.github.com>
2021-01-15 10:37:05 -0300

    Inseri validação de existencia de diretório/arquivos

--- UpdateRM.ps1
+++ UpdateRM.ps1
@@ -68,6 +68,17 @@ $command.StartExecutionTime
 $PSVersionTable
 
 <#===========================================================================================#>
+<# Validando diretórios e arquivos
+<#===========================================================================================#>
+test-path $DIRsiteRM
+test-path $RaizInstall
+test-path $RaizInstall\RM_$VersionRM.zip
+test-path $FileManual
+test-path $DIRsvcRM 
+test-path $DIRsvcScheduler
+#>
+
+<#===========================================================================================#>
 <# Verificando se os Recursos do Windows requeridos estão instalados
 <#===========================================================================================#>
 # Get-WindowsFeature -Name web-server, web-webserver,  web-common-http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Http-Redirect, Web-Health, Web-Http-Logging, Web-Log-Libraries, Web-Request-Monitor, Web-Http-Tracing, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering,  Web-App-Dev, Web-Net-Ext45, Web-AppInit, Web-ASP, Web-Asp-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Mgmt-Console, Web-Mgmt-Compat, Web-Metabase, Web-Scripting-Tools, MSMQ-Services, MSMQ-Server, MSMQ-Directory, MSMQ-HTTP-Support, MSMQ-Triggers,  NET-Framework-45-Features, NET-Framework-45-Core, NET-Framework-45-ASPNET, NET-WCF-Services45, NET-WCF-TCP-PortSharing45 | Select-Object Name , InstallState
