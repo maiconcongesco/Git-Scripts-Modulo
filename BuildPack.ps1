37ecb25bfafa500d55416266fbbbf1b7bf1844d7
Maicon <maiconcongesco@users.noreply.github.com>
2021-03-21 14:35:15 -0300

    Ajustes nos caminhos das pastas

--- BuildPack.ps1
+++ BuildPack.ps1
@@ -30,51 +30,51 @@ test-path $DIRPack\Manual_RM* # Verifica a existencia do zip com o Manual
 <#===========================================================================================#>
 <#  Pasta Binaries 1-4
 <#===========================================================================================#>
-New-Item -ItemType Directory -Force -Path $DIRPack\PackRiskManager\Binaries
-Move-Item "$DIRPack\RiskMananger*\packages\RiskManager.Service.zipx" $DIRPack\PackRiskManager\Binaries
-Move-Item "$DIRPack\RiskMananger*\packages\Modulo Scheduler Service.zipx" $DIRPack\PackRiskManager\Binaries
-Move-Item "$DIRPack\Modulo.ReportDesigner_10.0.0.3\packages\RM.ReportDesigner.Wix.msi" $DIRPack\PackRiskManager\Binaries
-Copy-Item "$FileModsic" $DIRPack\PackRiskManager\Binaries
+New-Item -ItemType Directory -Path $DIRPack\Pack\RM_$Version\Binaries -Verbose
+Move-Item "$DIRPack\RiskMananger*\packages\RiskManager.Service.zipx" $DIRPack\Pack\RM_$Version\Binaries -Verbose
+Move-Item "$DIRPack\RiskMananger*\packages\Modulo Scheduler Service.zipx" $DIRPack\Pack\RM_$Version\Binaries -Verbose
+Move-Item "$DIRPack\Modulo.ReportDesigner_10.0.0.3\packages\RM.ReportDesigner.Wix.msi" $DIRPack\Pack\RM_$Version\Binaries -Verbose
+Copy-Item "$FileModsic" $DIRPack\Pack\RM_$Version\Binaries -Verbose
 
 <#===========================================================================================#>
 <#  Pasta DevExpress 2-4
 <#===========================================================================================#>
-New-Item -ItemType Directory -Force -Path $DIRPack\PackRiskManager\DevExpress
-Copy-Item "$DevExpress\*" "$DIRPack\PackRiskManager\DevExpress" -Verbose -Force # Backup do Serviço do RiskManager
+New-Item -ItemType Directory -Path $DIRPack\Pack\RM_$Version\DevExpress -Verbose
+Copy-Item "$DevExpress\*" "$DIRPack\Pack\RM_$Version\DevExpress" -Verbose # Backup do Serviço do RiskManager
 
 <#===========================================================================================#>
 <#  Pasta Knowledges 3-4
 <#===========================================================================================#>
-New-Item -ItemType Directory -Force -Path $DIRPack\PackRiskManager\Knowledges 
-Copy-Item -Path "$Knowledges\*" -Destination "$DIRPack\PackRiskManager\Knowledges" -Force -Verbose
+New-Item -ItemType Directory -Path $DIRPack\Pack\RM_$Version\Knowledges -Verbose
+Copy-Item -Path "$Knowledges\*" -Destination "$DIRPack\Pack\RM_$Version\Knowledges" -Verbose
 
 <#===========================================================================================#>
 <#  Pasta Web.Applications 4-4
 <#===========================================================================================#>
-New-Item -ItemType Directory -Force -Path $DIRPack\PackRiskManager\Web.Applications
-Move-Item "$DIRPack\BCMBuild*\packages\BCM.zip" "$DIRPack\PackRiskManager\Web.Applications" -Force -Verbose
-Move-Item "$DIRPack\RiskMananger*\packages\RM.Portal.zip" "$DIRPack\PackRiskManager\Web.Applications" -Force -Verbose
-Move-Item "$DIRPack\RiskMananger*\packages\RM.WebApplication.zip" "$DIRPack\PackRiskManager\Web.Applications" -Force -Verbose
-Move-Item "$DIRPack\Workflow*\packages\Workflow.Services.Web.zip" "$DIRPack\PackRiskManager\Web.Applications" -Force -Verbose
-Move-Item "$DIRPack\DataAnalytics*" "$DIRPack\PackRiskManager\Web.Applications\DataAnalytics" -Force -Verbose
-Move-Item "$DIRPack\CCM.Build*" "$DIRPack\PackRiskManager\Web.Applications\MMI" -Force -Verbose
+New-Item -ItemType Directory -Path $DIRPack\Pack\RM_$Version\Web.Applications -Verbose
+Move-Item "$DIRPack\BCMBuild*\packages\BCM.zip" "$DIRPack\Pack\RM_$Version\Web.Applications" -Verbose
+Move-Item "$DIRPack\RiskMananger*\packages\RM.Portal.zip" "$DIRPack\Pack\RM_$Version\Web.Applications" -Verbose
+Move-Item "$DIRPack\RiskMananger*\packages\RM.WebApplication.zip" "$DIRPack\Pack\RM_$Version\Web.Applications" -Verbose
+Move-Item "$DIRPack\Workflow*\packages\Workflow.Services.Web.zip" "$DIRPack\Pack\RM_$Version\Web.Applications" -Verbose
+Move-Item "$DIRPack\DataAnalytics*" "$DIRPack\Pack\RM_$Version\Web.Applications\DataAnalytics" -Verbose
+Move-Item "$DIRPack\CCM.Build*" "$DIRPack\Pack\RM_$Version\Web.Applications\MMI" -Verbose
 
 <#===========================================================================================#>
 <#  Remover arquivos não utilizado por padrão
 <#===========================================================================================#>
-Remove-Item "$DIRPack\PackRiskManager\Web.Applications\MMI\packages\Modulo.RiskManager.CICC.Integrator.Synchronizer.zip" -Force -Verbose
-Remove-Item "$DIRPack\PackRiskManager\Web.Applications\DataAnalytics\DashboardDesignerInstallers\DashboardDesignerSetup_SQL32.exe" -Force -Verbose
-Remove-Item "$DIRPack\PackRiskManager\Web.Applications\DataAnalytics\DashboardDesignerInstallers\SqlLocalDB_EN_32bits.MSI" -Force -Verbose
-Remove-Item "$DIRPack\Modulo.ReportDesigner*" -Recurse -Force -Verbose
-Remove-Item "$DIRPack\BCMBuild*" -Recurse -Force -Verbose
-Remove-Item "$DIRPack\RiskMananger*" -Recurse -Force -Verbose
-Remove-Item "$DIRPack\Workflow*" -Recurse -Force -Verbose
+Remove-Item "$DIRPack\Pack\RM_$Version\Web.Applications\MMI\packages\Modulo.RiskManager.CICC.Integrator.Synchronizer.zip" -Verbose
+Remove-Item "$DIRPack\Pack\RM_$Version\Web.Applications\DataAnalytics\DashboardDesignerInstallers\DashboardDesignerSetup_SQL32.exe" -Verbose
+Remove-Item "$DIRPack\Pack\RM_$Version\Web.Applications\DataAnalytics\DashboardDesignerInstallers\SqlLocalDB_EN_32bits.MSI" -Verbose
+Remove-Item "$DIRPack\Modulo.ReportDesigner*" -Recurse -Verbose
+Remove-Item "$DIRPack\BCMBuild*" -Recurse -Verbose
+Remove-Item "$DIRPack\RiskMananger*" -Recurse -Verbose
+Remove-Item "$DIRPack\Workflow*" -Recurse -Verbose
 
 #===========================================================================================#
 # Criando pacote zipado (Seguindo ritos e padrões do antigo Suporte)
 #===========================================================================================#
 Add-Type -Assembly "System.IO.Compression.FileSystem"
-[System.IO.Compression.ZipFile]::CreateFromDirectory("$DIRPack\PackRiskManager", "$DIRPack\RM_$Version.zip")
+[System.IO.Compression.ZipFile]::CreateFromDirectory("$DIRPack\Pack", "$DIRPack\RM_$Version.zip")
 
 # Abrindo pasta
 Set-Location "$DIRPack"
